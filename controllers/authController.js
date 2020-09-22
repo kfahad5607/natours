8d17c3400e47617c78094c986fe0db1ab9adb091
Fahad <kfahad5607@gmail.com>
2020-09-22 19:05:34 +0530

    Added heroku config

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,7 +17,7 @@ const createSendToken = (user, statusCode, req, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,// browser cannot modify cookie
-        secure = req.secure || req.headers['x-forwarded-proto'] === 'https'
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
     };
     // Cookie will only work on HTTPS and not on HTTP
 
